223d1ea0143fb1fdde3267076a0fad586acde78b
qiaoqiaozou <2681059044@qq.com>
2024-02-05 17:46:52 +0200

    add a link for readme file

--- qiaoqiao_task/ex5.php
+++ qiaoqiao_task/ex5.php
@@ -5,7 +5,7 @@ include "header.php";
 <?php
 echo "This is a group project!"
 ?>
-
+<a href="https://github.com/qiaoqiaozou/Web_team5/blob/main/README.md">this is the readme link</a>
 <?php
 include "footer.php";
 ?>
